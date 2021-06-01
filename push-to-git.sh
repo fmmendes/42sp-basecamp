a769168aae298ea6f0e6cb53bdd07e4e46005db2
fimoraes <fimoraes@student.42sp.org.br>
2021-06-01 23:54:42 +0000

    done shell01

--- push-to-git.sh
+++ push-to-git.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Colors
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+NC='\033[0m' # No Color
+
+if [ -z $1 ]; then
+ printf "${Red}>> Você esqueceu a mensagem do commit ${NC}\n"
+ exit 0
+fi
+
 git add -A
 git commit -m "$1"
 git push
+clear
---
+++ shell/01/ex08/add_chelou_bkp.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+FT_NBR1=\\\'?\"\\\"\'\\
+FT_NBR2=rcrdmddd
+
+# traduzir o nbr1 na base '\"?!
+echo $FT_NBR1 
+echo $FT_NBR1 | tr "\'" '0' | tr '\\\"\?\!' "1234"
+echo ""
+# traduzir o nbr2 na base mrdoc
+echo $FT_NBR2
+echo $FT_NBR2 | tr "mrdoc" "01234"
+echo ""
+
+# Junta tudo e faz o calculo
+echo "obase=13;ibase=5;" $(echo $FT_NBR1 | tr "\'" '0' | tr '\\\"\?\!' "1234") + $(echo $FT_NBR2 | tr "mrdoc" "01234") | bc 
+echo ""
+
+#traduzindo o resultado par na base "gtaio luSnemf"(sim, o espaço conta)
+echo "obase=13;ibase=5;" $(echo $FT_NBR1 | tr "\'" '0' | tr '\\\"\?\!' "1234") + $(echo $FT_NBR2 | tr "mrdoc" "01234") | bc | tr '0123456789ABC' 'gtaio\ luSnemf'
+echo ""
+
+FT_NBR1=\\\"\\\"!\\\"\\\"!\\\"\\\"!\\\"\\\"!\\\"\\\"!\\\"\\\"
+FT_NBR2=dcrcmcmooododmrrrmorcmcrmomo
+echo "obase=13;ibase=5;" $(echo $FT_NBR1 | tr "\'" '0' | tr '\\\"\?\!' "1234") + $(echo $FT_NBR2 | tr "mrdoc" "01234") | bc | tr '0123456789ABC' 'gtaio\ luSnemf'
