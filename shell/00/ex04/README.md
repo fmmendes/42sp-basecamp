f504dcb619859328156ca825f010520d5c73cc9e
fimoraes <fimoraes@student.42sp.org.br>
2021-06-01 23:05:01 +0000

    WIP shell01

--- shell/00/ex04/README.md
+++ shell/00/ex04/README.md
@@ -1,7 +1,8 @@
 `git log --format=%H -5`
 
 `--format=<format>`  
-%H = commit hash
+> [%H = commit hash](https://git-scm.com/docs/git-log#Documentation/git-log.txt-emHem)
 
 `-<number>`  
 `-n <number> `
+`--max-count=<number>` 
---
+++ shell/01/ex08/add_chelou copy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+FT_NBR1=\\\'?\"\\\"\'\\
+FT_NBR2=rcrdmddd
+
+echo $FT_NBR1 
+echo $FT_NBR2
+
+echo $FT_NBR1 | tr "\'" '0' | tr '\\\"\?\!mrdoc' "123401234"
+
+echo $FT_NBR1 + $FT_NBR2
+
+echo "obase=13;ibase=5;" $FT_NBR1 + $FT_NBR2 | tr "\'" '0' | tr '\\\"\?\!mrdoc' "123401234" | sed 's/3/o/' | bc | tr '0123456789ABC' 'gtaio\ luSnemf'
+
+FT_NBR1=\\\"\\\"!\\\"\\\"!\\\"\\\"!\\\"\\\"!\\\"\\\"!\\\"\\\"
+FT_NBR2=dcrcmcmooododmrrrmorcmcrmomo
+
+echo $FT_NBR1 
+echo $FT_NBR2
+
+echo "obase=13;ibase=5;" $FT_NBR1 + $FT_NBR2 | tr "\'" '0' | tr '\\\"\?\!mrdoc' "123401234" | sed 's/3/o/' | bc | tr '0123456789ABC' 'gtaio\ luSnemf'
